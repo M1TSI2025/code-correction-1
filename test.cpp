8da0c2d7eb6af28a6c9ed71125f517a9cbf92181
Alexandre Agneray <122207242+alexagneray@users.noreply.github.com>
2025-09-17 21:49:15 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,17 +1,19 @@
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp"
-#include "main.cpp" // Inclut le code de l'étudiant
+#include "unit_test_framework.hpp"
+#include "exercice.hpp" // Inclut le code de l'étudiant
 
-TEST_CASE("Test de la fonction sommeTableau", "[somme]") {
+TEST(test_sommeTableau) {
     // Test 1: Tableau normal
     std::vector<int> tableau1 = {1, 2, 3};
-    REQUIRE(sommeTableau(tableau1) == 6);
+    ASSERT_TRUE(sommeTableau(tableau1) == 6);
 
     // Test 2: Tableau vide
     std::vector<int> tableau2 = {};
-    REQUIRE(sommeTableau(tableau2) == 0);
+    ASSERT_TRUE(sommeTableau(tableau2) == 0);
 
     // Test 3: Tableau avec des négatifs
     std::vector<int> tableau3 = {-1, 0, 1};
-    REQUIRE(sommeTableau(tableau3) == 0);
+    ASSERT_TRUE(sommeTableau(tableau3) == 0);
 }
+
+TEST_MAIN()
